7ff6b2539430f9ee81e6a1e3eb573dace18d8219
Dhivakarseetha-43 <baludhiva22@gmail.com>
2026-07-10 21:41:13 +0530

    ex3

---
+++ py2.py
@@ -0,0 +1,15 @@
+x=int(input("enter dec no:"))
+#bina=int(input("enter binary no:"))
+binres=[]
+#def dec2bin(x):
+while x!=0 or x!=1:
+    x%=2
+    binres.append(x)
+binres.reverse()
+
+print(binres)
+print("hi")
+        
+
+
+        
